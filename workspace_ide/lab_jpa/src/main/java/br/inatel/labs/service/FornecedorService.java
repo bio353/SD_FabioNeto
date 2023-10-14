ff5b0d5ae4761300877ee8e5c8783f2b122fea4f
Fabio Neto <bio353.dev@gmail.com>
2023-10-14 14:24:44 -0300

    Alterado FornecedorService com FornecedorRepository

--- workspace_ide/lab_jpa/src/main/java/br/inatel/labs/service/FornecedorService.java
+++ workspace_ide/lab_jpa/src/main/java/br/inatel/labs/service/FornecedorService.java
@@ -1,9 +1,9 @@
 package br.inatel.labs.service;
 
 import br.inatel.labs.entity.Fornecedor;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import br.inatel.labs.repository.FornecedorRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,25 +11,22 @@ import java.util.List;
 @Service
 @Transactional
 public class FornecedorService {
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private FornecedorRepository repository;
 
     public Fornecedor salvar(Fornecedor fornecedor) {
-        entityManager.merge(fornecedor);
-        return fornecedor;
+        return repository.save(fornecedor);
     }
 
     public Fornecedor buscarPeloId(Long id) {
-        return entityManager.find(Fornecedor.class, id);
+        return repository.findById(id).orElseThrow();
     }
 
     public List<Fornecedor> listar() {
-        String jpql = "SELECT f FROM Fornecedor f";
-        return entityManager.createQuery(jpql, Fornecedor.class).getResultList();
+        return repository.findAll();
     }
 
     public void remover(Fornecedor fornecedor) {
-        Fornecedor fornecedorParaRemover = entityManager.merge(fornecedor);
-        entityManager.remove(fornecedorParaRemover);
+        repository.delete(fornecedor);
     }
 }
