f670405f55ff1ca3f570c92b138c2c720747a12f
Fabio Neto <73840444+biossauro@users.noreply.github.com>
2023-11-30 12:26:36 -0300

    Removida dependência desnecessária

--- workspace_ide/lab_jpa/src/test/java/br/inatel/labs/labjpa/DataLoader.java
+++ workspace_ide/lab_jpa/src/test/java/br/inatel/labs/labjpa/DataLoader.java
@@ -10,7 +10,6 @@ import br.inatel.labs.service.ProdutoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
