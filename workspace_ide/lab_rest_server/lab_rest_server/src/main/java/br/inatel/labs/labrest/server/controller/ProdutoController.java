db9207e4f3220eb4c0856274a9a327333e69a219
Fabio Neto <73840444+biossauro@users.noreply.github.com>
2023-11-30 12:34:07 -0300

    Adicionado código da aula

---
+++ workspace_ide/lab_rest_server/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/controller/ProdutoController.java
@@ -0,0 +1,53 @@
+package br.inatel.labs.labrest.server.controller;
+
+import br.inatel.labs.labrest.server.model.Produto;
+import br.inatel.labs.labrest.server.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("produto")
+public class ProdutoController {
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public List<Produto> getProdutos() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto getProdutoById(@PathVariable("id") Long produtoId) {
+        Optional<Produto> opProduto = service.findById(produtoId);
+        if (opProduto.isPresent()) {
+            return opProduto.get();
+        }
+        String msg = String.format("Nenhum produto encontrado com id [%s]", produtoId);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Produto postProduto(@RequestBody Produto p) {
+        Produto produtoCriado = service.create(p);
+        return produtoCriado;
+    }
+
+    @PutMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void putProduto(@RequestBody Produto p) {
+        service.update(p);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteProduto(@PathVariable("id") Long produtoId) {
+        Produto produtoEncontrado = this.getProdutoById(produtoId);
+        service.remove(produtoEncontrado);
+    }
+}
---
+++ workspace_ide/lab_rest_server/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/model/Produto.java
@@ -0,0 +1,55 @@
+package br.inatel.labs.labrest.server.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+    private Long id;
+
+    private String descricao;
+
+    private Double preco;
+
+    public Produto(Long id, String descricao, Double preco) {
+        super();
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return Objects.equals(getId(), produto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
